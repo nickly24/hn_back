087d53dd5d26bb8da2900b9a2fccfebe82126987
nickly24 <nikolaypribysh@mail.ru>
2025-08-21 13:17:34 +0300

    ff

--- main.py
+++ main.py
@@ -947,6 +947,6 @@ def delete_system_canban_task(task_id):
         connection.close()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=80)
 
 # Для запуска через gunicorn: gunicorn main:app --timeout 60 --bind 0.0.0.0:$PORT
